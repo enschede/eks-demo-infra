3404f793eb54679677b917e7c4f33c08a6a9a029
Marc Enschede <marc@marcenschede.nl>
2023-12-22 23:50:02 +0100

    Added prometheus to have Route53 entries removed too

--- stop.sh
+++ stop.sh
@@ -2,10 +2,11 @@
 
 flux suspend kustomization --context console@eks-demo.us-west-2.eksctl.io flux-system
 flux delete kustomization --context console@eks-demo.us-west-2.eksctl.io --silent eks-demo-app
+flux delete kustomization --context console@eks-demo.us-west-2.eksctl.io --silent prometheus
 sleep 1
 flux delete kustomization --context console@eks-demo.us-west-2.eksctl.io --silent eks-demo-app-db
 sleep 1
 flux delete kustomization --context console@eks-demo.us-west-2.eksctl.io --silent eks-demo-app-crossplane
-sleep 60
+sleep 10
 
 eksctl delete cluster eks-demo --wait
